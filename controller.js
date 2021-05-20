f8683f15105cb51d66146715a4d987c77de7c4dc
lilach <lilachgro@gmail.com>
2021-05-20 22:23:11 +0300

    creating the folders and example for the controller.js

---
+++ controller.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const fileUpload = require("express-fileupload")
+//searchInFile not existing in our project
+const model = require("./model/searchInFile")
+
+const app = express()
+
+app.use(express.urlencoded({ extended: false }))
+app.use(fileUpload())
+app.use(express.static("./view"))
+
+app.get("/", function (req, res) {
+    res.sendFile("index.html")
+});
+
+//not needed in our project
+app.post("/search", function (req, res) {
+    var key = req.body.key
+    res.write("search for " + key + ":\n")
+    if (req.files) {
+        var file = req.files.text_file
+        var result = model.searchText(key, file.data.toString())
+        res.write(result)
+    }
+    res.end()
+});
+
+app.listen(process.env.PORT || 8080)
