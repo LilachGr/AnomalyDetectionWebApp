b1878c63d7f3d2604a1004c4a0bc8b1787b4cfd4
lilach <lilachgro@gmail.com>
2021-05-26 22:51:16 +0300

    updating model folder

--- controller.js
+++ controller.js
@@ -1,26 +1,28 @@
 const express = require("express");
 //express-fileupload - not download to the npm
-//const fileUpload = require("express-fileupload")
+const fileUpload = require("express-fileupload")
 //searchInFile not existing in our project
-//const model = require("./model/searchInFile")
-
-const modelDll = require("./model/AlgorithmFunctions")
+const model = require("./model/searchInFile")
 
 const app = express()
 
 app.use(express.urlencoded({ extended: false }))
-//app.use(fileUpload())
+app.use(fileUpload())
 app.use(express.static("./view"))
 
 app.get("/", function (req, res) {
-   res.sendFile("index.html")
+    res.sendFile("index.html")
 });
 
 //not needed in our project
 app.post("/search", function (req, res) {
-    res.write("hello world")
-    modelDll.algorithmForFindingAnomalies(0, "D:\\biu_exercise\\advandedProgramming2\\tryNodeJS2\\NodejsWebApp1\\NodejsWebApp1\\reg_flight.csv", "D:\\biu_exercise\\advandedProgramming2\\tryNodeJS2\\NodejsWebApp1\\NodejsWebApp1\\anomaly_flight.csv", "./anomalies.txt")
-    modelDll.algorithmForFindingAnomalies(1, "D:\\biu_exercise\\advandedProgramming2\\tryNodeJS2\\NodejsWebApp1\\NodejsWebApp1\\reg_flight.csv", "D:\\biu_exercise\\advandedProgramming2\\tryNodeJS2\\NodejsWebApp1\\NodejsWebApp1\\anomaly_flight.csv", "./anomalies2.txt")
+    var key = req.body.key
+    res.write("search for " + key + ":\n")
+    if (req.files) {
+        var file = req.files.text_file
+        var result = model.searchText(key, file.data.toString())
+        res.write(result)
+    }
     res.end()
 });
 
